4eaa193aff266201bcd3678790f3161195d17e7f
Lisa Steinmann <lisa.steinmann@rub.de>
2026-07-04 13:49:31 +0200

    check for duplicate keys

--- src/data_checker.py
+++ src/data_checker.py
@@ -1,5 +1,6 @@
 from typing import List, Dict, Any, Tuple
 import pandas as pd
+import datetime
 
 class DataChecker:
     """
@@ -15,22 +16,45 @@ class DataChecker:
         """
         self.items = items
         self.logfile = logfile
-        self.log = []
+        log = open(logfile, "w")
+        log.writelines(f"Processing date and time: {datetime.datetime.now()}\n\n")
+        log.close()
         print("Initialized the DataChecker.")
     
+    def log(self, msg):
+        print(msg)
+        log = open(self.logfile, "a")
+        log.write(f"{msg} \n\n")
+        log.close()
+
+
     def find_duplicate_citation_keys(self):
+        self.log("Checking for duplicate citationKeys...")
         citation_keys = []
         for item in self.items:
             key = item.get('key', '')
             item_data = item.get('data', '')
-            citation_key = item_data.get('citationKey', '')
-            citation_keys.append({
-                'Key': key,
-                'citationKey': citation_key
-            })
+            if item_data.get('itemType') == 'note': 
+                pass
+            else: 
+                meta = item.get('meta')
+                creator = meta.get('createdByUser')
+                citation_keys.append({
+                    'Key': key,
+                    'citationKey': item_data.get('citationKey', ''),
+                    'dateAdded': item_data.get('dateAdded', ''),
+                    'title': item_data.get('title', ''),
+                    'createdBy': creator.get('username')
+                })
         citation_keys = pd.DataFrame(citation_keys)
         duplicates = citation_keys['citationKey'].duplicated(keep=False)
-        print(duplicates)
+        duplicate_rows = citation_keys[duplicates]
+        if len(duplicate_rows.index) > 0:
+            self.log(f"Found {len(duplicate_rows.index)} duplicate citationKeys:")
+            self.log(duplicate_rows)
+            self.log("You need to check and fix these.")
+        else: 
+            self.log("There are no duplicate citationKeys - excellent.")
 
 
 # Example usage:
@@ -41,3 +65,4 @@ if __name__ == "__main__":
         data = json.load(file)
     
     data_checker = DataChecker(data)
+    data_checker.find_duplicate_citation_keys()
